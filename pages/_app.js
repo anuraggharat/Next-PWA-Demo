2f5837f20f979be5e43f1bff21bdee4472caa303
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2022-02-11 13:44:44 +0530

    PWA Added

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        // Responsive meta tag
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        // bootstrap CDN
+        
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
           rel="stylesheet"
